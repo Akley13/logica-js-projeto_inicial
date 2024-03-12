d853f9ac60a70bab8b9c2c88f9c890059c1ccff9
Akley Alves <akleyalvescr@gmail.com>
2024-03-12 10:39:01 -0300

    Desafios compridos da Alura!

---
+++ Desafios/1.js
@@ -0,0 +1,18 @@
+alert("Boas-vindas ao nosso site!");
+
+alert ("Erro! Preencha todos os campos.");
+
+const mensagemDeErro = "Erro! Preencha todos os campos";
+
+alert(mensagemDeErro);
+
+const nomeCompleto = prompt("Qual seu nome completo? ");
+
+const idade1 = prompt("E a sua idade? ");
+
+if (idade1 >= 18) {
+    alert("Pode tirar habilitação!");
+} else {
+    alert("Habilitação apenas para maiores de idade!");
+}
+
--- app.js
+++ app.js
@@ -2,6 +2,12 @@ alert("Olá mundo, Sejam bem-vindos ao jogo dos números secretos!");
 
 const numeroSorteado = 29;
 
-const numeroSorteado = 30;
+var resposta = prompt("Escolha um número entre 1 e 30: ");
 
-prompt("Escolha um número entre 1 e 30: ");  
+if (resposta == numeroSorteado) {
+    console.log("Acertouuuuuuuuu");
+    alert("Acertou, Parabéns! ")
+} else {
+    console.log("Errado, tente novamente!");
+    alert("Errado, tente novamente! ")
+}
